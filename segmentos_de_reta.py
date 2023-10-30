1a96e7a4e2a7632996e97a23a9a7844dce64a182
Caio Pessoa <caiopessoa1298@gmail.com>
2023-10-30 14:10:24 -0300

    add `segmentos_de_reta`

---
+++ segmentos_de_reta.py
@@ -0,0 +1,14 @@
+class Retas:
+    px = (int, int)
+    py = (int, int)
+    def __init__(self, px, py):
+        self.px = px
+        self.py = py
+        
+
+retas = [
+    Retas((10, 10), (20, 20)),
+    Retas((10, 10), (20, 20)),
+    Retas((10, 10), (20, 20)),
+    Retas((10, 10), (20, 20)),
+]
